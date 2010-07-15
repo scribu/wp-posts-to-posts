28b0a54c9498b0f6befbc0835dfc5fa102550273
scribu <scribu@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-15 15:54:04 +0000

    move p2p_list_connected() to api.php
    git-svn-id: http://plugins.svn.wordpress.org/posts-to-posts/trunk@263767 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- api.php
+++ api.php
@@ -1,9 +1,5 @@
 <?php
 
-//
-// API functions deal only with connection manipulation
-//
-
 /**
  * Register a connection between two post types. 
  * This creates the appropriate meta box in the admin edit screen
@@ -158,5 +154,53 @@ function p2p_get_connected( $post_type, $direction, $post_id, $grouped = false )
 		return array( $post_type => $connections );
 
 	return $connections;
+}
+
+/**
+ * Display the list of connected posts
+ *
+ * @param string $post_type The post type of the connected posts.
+ * @param string $direction The direction of the connection. Can be 'to' or 'from'
+ * @param int $post_id One end of the connection
+ * @param callback(WP_Query) $callback the function used to do the actual displaying
+ */
+function p2p_list_connected( $post_type = 'any', $direction = 'from', $post_id = '', $callback = '' ) {
+	if ( !$post_id )
+		$post_id = get_the_ID();
+
+	$connected_post_ids = p2p_get_connected( $post_type, $direction, $post_id );
+
+	if ( empty( $connected_post_ids ) )
+		return;
+
+	$args = array(
+		'post__in' => $connected_post_ids,
+		'post_type'=> $post_type,
+		'nopaging' => true,
+	);
+	$query = new WP_Query( $args );
+
+	if ( empty( $callback ) )
+		$callback = '_p2p_list_connected';
+
+	call_user_func( $callback, $query );
+
+	wp_reset_postdata();
+}
+
+/**
+ * The default callback for p2p_list_connected()
+ * Lists the posts as an unordered list
+ *
+ * @param WP_Query
+ */
+function _p2p_list_connected( $query ) {
+	if ( $query->have_posts() ) :
+		echo '<ul>';
+		while ( $query->have_posts() ) : $query->the_post();
+			echo html( 'li', html_link( get_permalink( get_the_ID() ), get_the_title() ) );
+		endwhile;
+		echo '</ul>';
+	endif;
 }
 
