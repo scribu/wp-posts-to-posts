76854bbbc6cc3bdb228108a6277fa03f50930151
Alex Ciobica <alex.ciobica@gmail.com>
2011-03-30 20:56:45 +0300

    hide pagination if 1 page

--- ui/ui.js
+++ ui/ui.js
@@ -148,19 +148,25 @@ $('.p2p-add-new').each(function() {
 
 			$metabox.find('.p2p-search').find('.p2p-notice').remove();
 
+			var pagination = $metabox.find('.p2p-nav');
+			pagination.hide();
+			
 			if ( 'undefined' === typeof response.rows ) {
 				$metabox.find('.p2p-search').append('<p class="p2p-notice">' + response.msg + '</p>');
 				$results.hide()
 					.find('tbody').html('');
-				$metabox.find('.p2p-nav').hide();
+				
 			} else {
 				$results.show()
 					.find('tbody').html(response.rows);
 
 				total_pages = response.pages;
-
+				
+				if ( total_pages > 1 ) {
+					$metabox.find('p2p-nav').show();					
+				} 
+				
 				// update pagination
-				$metabox.find('.p2p-nav').show();
 				if ( 1 === current_page ) {
 					$metabox.find('.p2p-prev').addClass('inactive');
 				} else {
