7dadd3393c3b22e3d48d38199fffeee00577dbdc
scribu <mail@scribu.net>
2011-07-12 13:35:04 +0300

    bump scripts version

--- posts-to-posts.php
+++ posts-to-posts.php
@@ -26,7 +26,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define( 'P2P_PLUGIN_VERSION', '0.8-alpha' );
+define( 'P2P_PLUGIN_VERSION', '0.8-alpha2' );
 
 require dirname( __FILE__ ) . '/scb/load.php';
 
