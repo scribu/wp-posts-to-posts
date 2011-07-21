47aa0960947ef73328c0820198277591705d7528
scribu <mail@scribu.net>
2011-07-21 17:25:36 +0300

    refactor filter_ctypes() into make_box() so that only the required box is instantiated in an ajax request

--- ui.php
+++ ui.php
@@ -76,13 +76,11 @@ class P2P_Connection_Types {
 	}
 
 	static function add_meta_boxes( $from ) {
-		$filtered = self::filter_ctypes( $from );
+		foreach ( self::$ctypes as $box_id => $args ) {
+			$box = self::make_box( $box_id, $from );
 
-		if ( empty( $filtered ) )
-			return;
-
-		foreach ( $filtered as $ctype ) {
-			$ctype->_register( $from );
+			if ( $box )
+				$box->_register( $from );
 		}
 	}
 
@@ -100,13 +98,9 @@ class P2P_Connection_Types {
 	function wp_ajax_p2p_box() {
 		check_ajax_referer( P2P_BOX_NONCE, 'nonce' );
 
-		$box_id = absint( $_REQUEST['box_id'] );
-		$boxes = self::filter_ctypes( $_REQUEST['post_type'] );
-
-		if ( !isset( $boxes[ $box_id ] ) )
+		$box = self::make_box( $_REQUEST['box_id'], $_REQUEST['post_type'] );
+		if ( !$box )
 			die(0);
-
-		$box = $boxes[ $box_id ];
 
 		$ptype_obj = get_post_type_object( $box->from );
 		if ( !current_user_can( $ptype_obj->cap->edit_posts ) )
@@ -117,29 +111,31 @@ class P2P_Connection_Types {
 		$box->$method();
 	}
 
-	private static function filter_ctypes( $post_type ) {
-		$r = array();
+	private static function make_box( $box_id, $post_type ) {
+		if ( !isset( self::$ctypes[ $box_id ] ) )
+			return false;
 
-		foreach ( self::$ctypes as $box_id => $args ) {
-			$direction = false;
+		$args = self::$ctypes[ $box_id ];
 
-			if ( $args['reciprocal'] && $post_type == $args['from'] && $args['from'] == $args['to'] ) {
-				$direction = 'any';
-			} elseif ( $args['reciprocal'] && $post_type == $args['to'] ) {
-				$direction = 'to';
-			} elseif ( $post_type == $args['from'] ) {
-				$direction = 'from';
-			} else {
-				continue;
-			}
+		$direction = self::get_direction( $post_type, $args );
+		if ( !$direction )
+			return false;
 
-			if ( !$direction )
-				continue;
+		return new $args['box']( $args, $direction, $box_id );
+	}
 
-			$r[ $box_id ] = new $args['box']($args, $direction, $box_id);
+	private static function get_direction( $post_type, $args ) {
+		$direction = false;
+
+		if ( $args['reciprocal'] && $post_type == $args['from'] && $args['from'] == $args['to'] ) {
+			$direction = 'any';
+		} elseif ( $args['reciprocal'] && $post_type == $args['to'] ) {
+			$direction = 'to';
+		} elseif ( $post_type == $args['from'] ) {
+			$direction = 'from';
 		}
 
-		return $r;
+		return $direction;
 	}
 }
 scbHooks::add( 'P2P_Connection_Types' );
