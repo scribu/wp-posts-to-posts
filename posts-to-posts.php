8b0be9bd52b1c4003a3bb095f854e4f66afdf23f
scribu <mail@scribu.net>
2011-10-24 09:10:22 +0300

    bump script version to 0.9.6-alpha

--- posts-to-posts.php
+++ posts-to-posts.php
@@ -26,7 +26,7 @@ You should have received a copy of the GNU General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-define( 'P2P_PLUGIN_VERSION', '0.9.5' );
+define( 'P2P_PLUGIN_VERSION', '0.9.6-alpha' );
 
 define( 'P2P_TEXTDOMAIN', 'posts-to-posts' );
 
