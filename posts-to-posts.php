ca397fa065f758f9de04d8f904ffa2eb7d60ab9d
scribu <mail@scribu.net>
2011-11-11 16:31:17 +0200

    update email address

--- posts-to-posts.php
+++ posts-to-posts.php
@@ -10,7 +10,7 @@ Text Domain: posts-to-posts
 Domain Path: /lang
 
 
-Copyright (C) 2010-2011 Cristi Burcă (scribu@gmail.com)
+Copyright (C) 2010-2011 Cristi Burcă (mail@scribu.net)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
