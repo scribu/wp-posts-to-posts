c392f70996f483fd71587e518c0959b1c774aeb5
scribu <mail@scribu.net>
2011-11-25 14:32:54 +0200

    disable paging when getting posts to import

--- admin/tools.php
+++ admin/tools.php
@@ -24,7 +24,10 @@ class P2P_Tools extends scbAdminPage {
 		$n = 0;
 
 		foreach ( P2P_Connection_Type_Factory::get_all_instances() as $p2p_type => $ctype ) {
-			$args = $ctype->set_direction( 'any' )->get_connected_args( 'any', array( 'cache_results' => false ) );
+			$args = $ctype->set_direction( 'any' )->get_connected_args( 'any', array(
+				'cache_results' => false,
+				'nopaging' => true
+			) );
 			unset( $args['p2p_type'] );
 
 			foreach ( get_posts( $args ) as $post ) {
