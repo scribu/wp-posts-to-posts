f9823121d7f6c043744691c1907c8be53b8b3346
scribu <mail@scribu.net>
2012-01-15 01:27:59 +0200

    fix Version header

--- posts-to-posts.php
+++ posts-to-posts.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: Posts 2 Posts
 Description: Create many-to-many relationships between all types of posts.
-Version: 1.2-alpha
+Version: 1.1.4
 Author: scribu
 Author URI: http://scribu.net/
 Plugin URI: http://scribu.net/wordpress/posts-to-posts
