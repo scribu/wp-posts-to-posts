0b8719287988be8ea450d445a4e041dc0de71fde
scribu <mail@scribu.net>
2012-04-30 18:50:32 +0300

    replace copyright notice with license headers

--- posts-to-posts.php
+++ posts-to-posts.php
@@ -8,22 +8,8 @@ Author URI: http://scribu.net/
 Plugin URI: http://scribu.net/wordpress/posts-to-posts
 Text Domain: posts-to-posts
 Domain Path: /lang
-
-
-Copyright (C) 2010-2012 Cristi Burcă (mail@scribu.net)
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program. If not, see <http://www.gnu.org/licenses/>.
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
 */
 
 define( 'P2P_PLUGIN_VERSION', '1.3-beta' );
