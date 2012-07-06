737649f3e39c9a0d40b12896a7c03f278d485d59
scribu <mail@scribu.net>
2012-07-07 00:36:36 +0300

    change 'Create connections:' label based on cardinality. fixes #173

--- admin/box.php
+++ admin/box.php
@@ -94,12 +94,16 @@ class P2P_Box {
 	}
 
 	protected function render_create_connections( $post ) {
-		$data = array(
-			'label' => __( 'Create connections:', P2P_TEXTDOMAIN )
-		);
+		$data = array();
 
-		if ( 'one' == $this->ctype->get_opposite( 'cardinality' ) && !empty( $this->connected_items ) )
-			$data['hide'] = 'style="display:none"';
+		if ( 'one' == $this->ctype->get_opposite( 'cardinality' ) ) {
+			$data['label'] = __( 'Create connection:', P2P_TEXTDOMAIN );
+
+			if ( !empty( $this->connected_items ) )
+				$data['hide'] = 'style="display:none"';
+		} else {
+			$data['label'] = __( 'Create connections:', P2P_TEXTDOMAIN );
+		}
 
 		// Search tab
 		$tab_content = P2P_Mustache::render( 'tab-search', array(
