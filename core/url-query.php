1d09a2b6d11bf65351d12ec0859271a1028bc1c2
scribu <mail@scribu.net>
2012-07-07 16:19:02 +0300

    make P2P_URL_Query affect the users screen

--- core/url-query.php
+++ core/url-query.php
@@ -2,16 +2,38 @@
 
 class P2P_URL_Query {
 
-	function init() {
-		add_filter( 'query_vars', array( __CLASS__, 'query_vars' ) );
+	static function get_custom_qv() {
+		return array( 'connected_type', 'connected_items', 'connected_direction' );
 	}
 
-	function query_vars( $public_qv ) {
-		return array_merge( $public_qv, array(
-			'connected_type',
-			'connected_items',
-			'connected_direction',
-		) );
+	static function init() {
+		add_filter( 'query_vars', array( __CLASS__, 'query_vars' ) );
+
+		if ( is_admin() )
+			add_action( 'pre_user_query', array( __CLASS__, 'user_query' ), 9 );
+	}
+
+	// Make the query vars public
+	static function query_vars( $public_qv ) {
+		return array_merge( $public_qv, self::get_custom_qv() );
+	}
+
+	// Add the query vars to the global user query (on the user admin screen)
+	static function user_query( $query ) {
+		// Restrict to users screen
+		if ( 'users' != get_current_screen()->id )
+			return;
+
+		// Don't overwrite capturing query
+		if ( isset( $query->_p2p_capture ) )
+			return;
+
+		// Don't overwrite existing P2P query
+		if ( isset( $query->query_vars['connected_type'] ) )
+			return;
+
+		_p2p_append( $query->query_vars, wp_array_slice_assoc( $_GET,
+			P2P_URL_Query::get_custom_qv() ) );
 	}
 }
 
