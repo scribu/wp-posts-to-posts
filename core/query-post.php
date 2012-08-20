4f911cd5b30d6b42704511f109d03638a79d3606
herewithme <amaury@beapi.fr>
2012-08-20 21:32:22 +0200

    Split main query, get ID and after contents (also with P2)
    In reference : https://github.com/scribu/wp-posts-to-posts/issues/219

--- core/query-post.php
+++ core/query-post.php
@@ -6,6 +6,7 @@ class P2P_Post_Query {
 		add_action( 'parse_query', array( __CLASS__, 'parse_query' ), 20 );
 		add_filter( 'posts_clauses', array( __CLASS__, 'posts_clauses' ), 20, 2 );
 		add_filter( 'posts_request', array( __CLASS__, 'capture' ), 999, 2 );
+		add_filter( 'posts_results', array( __CLASS__, 'posts_results'), 1, 2 );
 		add_filter( 'the_posts', array( __CLASS__, 'cache_p2p_meta' ), 20, 2 );
 	}
 
@@ -34,6 +35,10 @@ class P2P_Post_Query {
 		if ( !isset( $wp_query->_p2p_query ) )
 			return $clauses;
 
+		// Alter WP clauses for get only ID instead all datas
+		$clauses['fields'] = "$wpdb->posts.ID";
+
+		// Add P2P to clauses
 		return $wp_query->_p2p_query->alter_clauses( $clauses, "$wpdb->posts.ID" );
 	}
 
@@ -46,6 +51,37 @@ class P2P_Post_Query {
 		$wp_query->_p2p_sql = $request;
 
 		return '';
+	}
+
+	static function posts_results( $posts, $wp_query ) {
+		global $wpdb;
+		
+		if ( !isset( $wp_query->_p2p_query ) || empty($posts) )
+			return $posts;
+		
+		// Get posts IDs for get contents
+		$_posts = array();
+		foreach( $posts as $post ) {
+			$_posts[] = $post->ID;
+		}
+		
+		// setup posts data
+		_prime_post_caches( $_posts, $wp_query->query_vars['update_post_term_cache'], $wp_query->query_vars['update_post_meta_cache'] );
+		$_posts = array_map( 'get_post', $_posts );
+		
+		// Put ID on key
+		foreach( $_posts as $key => $_post ) {
+			unset($_posts[$key]);
+			$_posts[$_post->ID] = $_post;
+		}
+		
+		// Merge datas
+		$wp_query->posts = array();
+		foreach( $posts as $post ) {
+			$wp_query->posts[] = (object) array_merge((array) $_posts[$post->ID], (array) $post);
+		}
+		
+		return $wp_query->posts;
 	}
 
 	/**
