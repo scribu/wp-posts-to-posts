c9f8ae1f9131a76090e994a8e18e6945c9e5ef44
scribu <mail@scribu.net>
2012-09-12 01:00:10 +0300

    move other connection type classes to core/type.php

--- core/indeterminate-type.php
+++ core/indeterminate-type.php
@@ -1,38 +1,5 @@
 <?php
 
-class P2P_Indeterminate_Connection_Type extends P2P_Connection_Type {
-
-	protected $directed_class = 'P2P_Indeterminate_Directed_Connection_Type';
-
-	protected $arrow = '&harr;';
-
-	protected function choose_direction( $direction ) {
-		return 'from';
-	}
-
-	function _directions_for_admin( $direction, $show_ui ) {
-		return parent::_directions_for_admin( 'any', $show_ui );
-	}
-}
-
-
-class P2P_Reciprocal_Connection_Type extends P2P_Indeterminate_Connection_Type {
-
-	protected function choose_direction( $direction ) {
-		return 'any';
-	}
-
-	function _directions_for_admin( $direction, $show_ui ) {
-		if ( $show_ui )
-			$directions = array( 'any' );
-		else
-			$directions = array();
-
-		return $directions;
-	}
-}
-
-
 class P2P_Indeterminate_Directed_Connection_Type extends P2P_Directed_Connection_Type {
 
 	protected function recognize( $arg ) {
