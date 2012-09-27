8b7863164a7fcf47acb6c649f360b1dc5ce3aae0
scribu <mail@scribu.net>
2012-09-27 23:13:15 +0300

    use 'p2p_registered_connection_type' for registering missing post types

--- debug-utils.php
+++ debug-utils.php
@@ -2,16 +2,14 @@
 
 // Tools for testing and debugging P2P
 
-add_filter( 'p2p_connection_type_args', '_p2p_register_missing_post_types', 10, 2 );
+add_action( 'p2p_registered_connection_type', '_p2p_register_missing_post_types' );
 
-function _p2p_register_missing_post_types( $args, $sides ) {
-	foreach ( _p2p_extract_post_types( $sides ) as $ptype ) {
+function _p2p_register_missing_post_types( $ctype ) {
+	foreach ( _p2p_extract_post_types( $ctype->side ) as $ptype ) {
 		if ( !post_type_exists( $ptype ) ) {
 			_p2p_generate_post_type( $ptype );
 		}
 	}
-
-	return $args;
 }
 
 function _p2p_generate_post_type( $slug ) {
