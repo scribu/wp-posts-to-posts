9be7d637d227cd624880eadd65d0b67dc694f570
scribu <mail@scribu.net>
2012-11-24 02:45:55 +0200

    command: update call for generating posts

--- command.php
+++ command.php
@@ -30,8 +30,8 @@ class P2P_CLI_Command extends WP_CLI_Command {
 
 		if ( isset( $assoc_args['items'] ) ) {
 			foreach ( _p2p_extract_post_types( $ctype->side ) as $ptype ) {
-				$command = array( 'wp', 'generate', 'posts' );
-				$assoc_args = array( 'type' => $ptype );
+				$command = array( 'wp', 'post', 'generate' );
+				$assoc_args = array( 'post_type' => $ptype );
 
 				WP_CLI::launch( WP_CLI::compose_args( $command, $assoc_args ) );
 			}
