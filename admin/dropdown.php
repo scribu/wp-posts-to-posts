d3f03cb391b0311376d23982177c8d888f658484
scribu <mail@scribu.net>
2012-11-27 16:42:17 +0200

    handle query for user dropdowns

--- admin/dropdown.php
+++ admin/dropdown.php
@@ -14,7 +14,7 @@ abstract class P2P_Dropdown {
 		echo $this->render_dropdown();
 	}
 
-	function render_dropdown() {
+	protected function render_dropdown() {
 		$direction = $this->ctype->flip_direction()->get_direction();
 
 		return scbForms::input( array(
@@ -23,6 +23,24 @@ abstract class P2P_Dropdown {
 			'choices' => self::get_choices( $this->ctype ),
 			'text' => $this->title,
 		), $_GET );
+	}
+
+	protected static function get_qv() {
+		if ( !isset( $_GET['p2p'] ) )
+			return array();
+
+		$args = array();
+
+		$tmp = reset( $_GET['p2p'] );
+
+		$args['connected_type'] = key( $_GET['p2p'] );
+
+		list( $args['connected_direction'], $args['connected_items'] ) = each( $tmp );
+
+		if ( !$args['connected_items'] )
+			return array();
+
+		return $args;
 	}
 
 	protected static function get_choices( $directed ) {
@@ -53,21 +71,7 @@ class P2P_Dropdown_Post extends P2P_Dropdown {
 	}
 
 	static function massage_query( $request ) {
-		if ( isset( $_GET['p2p'] ) ) {
-			$args = array();
-
-			$tmp = reset( $_GET['p2p'] );
-
-			$args['connected_type'] = key( $_GET['p2p'] );
-
-			list( $args['connected_direction'], $args['connected_items'] ) = each( $tmp );
-
-			if ( $args['connected_items'] ) {
-				_p2p_append( $request, $args );
-			}
-		}
-
-		return $request;
+		return array_merge( $request, self::get_qv() );
 	}
 }
 
@@ -77,10 +81,23 @@ class P2P_Dropdown_User extends P2P_Dropdown_Post {
 	function __construct( $directed, $title ) {
 		parent::__construct( $directed, $title );
 
+		add_action( 'pre_user_query', array( __CLASS__, 'massage_query' ), 9 );
+
 		add_action( 'restrict_manage_users', array( $this, 'show_dropdown' ) );
 	}
 
-	function render_dropdown() {
+	static function massage_query( $query ) {
+		if ( isset( $query->_p2p_capture ) )
+			return;
+
+		// Don't overwrite existing P2P query
+		if ( isset( $query->query_vars['connected_type'] ) )
+			return;
+
+		_p2p_append( $query->query_vars, self::get_qv() );
+	}
+
+	protected function render_dropdown() {
 		return html( 'div', array(
 			'style' => 'float: right; margin-left: 16px'
 		),
