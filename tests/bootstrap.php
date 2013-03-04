42ea9632c9736e3a63de4ab51644b6f366866a28
scribu <mail@scribu.net>
2013-03-05 00:12:26 +0200

    use WP_TESTS_DIR instead of having wp-tests-lib as a submodule

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,11 +1,11 @@
 <?php
 
-require_once dirname( __FILE__ ) . '/lib/functions.php';
+require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
 tests_add_filter( 'muplugins_loaded', function() {
 	require dirname( __FILE__ ) . '/../posts-to-posts.php';
 	require dirname( __FILE__ ) . '/../debug-utils.php';
 } );
 
-require dirname( __FILE__ ) . '/lib/bootstrap.php';
+require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
 
