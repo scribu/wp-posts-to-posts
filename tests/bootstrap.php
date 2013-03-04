5c0ecdf7848db40beddddb4c30790a26ceb4cd6d
scribu <mail@scribu.net>
2013-03-05 00:06:46 +0200

    load debug-utils.php in test bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,6 +4,7 @@ require_once dirname( __FILE__ ) . '/lib/functions.php';
 
 tests_add_filter( 'muplugins_loaded', function() {
 	require dirname( __FILE__ ) . '/../posts-to-posts.php';
+	require dirname( __FILE__ ) . '/../debug-utils.php';
 } );
 
 require dirname( __FILE__ ) . '/lib/bootstrap.php';
